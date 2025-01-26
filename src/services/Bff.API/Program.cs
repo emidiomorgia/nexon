9d3a3efb37fea66ab400d26ddb19eea56f799643
Emidio Morgia <emidio@morgia.info>
2025-01-26 22:31:07 +0100

    configure database connection and add environment variable support in Bff.API

--- src/services/Bff.API/Program.cs
+++ src/services/Bff.API/Program.cs
@@ -1,8 +1,19 @@
+using Bff.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())  // imposta la directory di base per i file di configurazione
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables(); // Aggiungi le variabili di ambiente (compreso il file .env che le definisce)
+
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
